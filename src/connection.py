434a83ddeb43982a6b1b3de55cc24c05dab3a243
Tamas SmahajcsikSzabo <tamas.smahajcsikszabo@outlook.hu>
2024-08-31 21:52:31 +0200

    update

--- src/connection.py
+++ src/connection.py
@@ -1,17 +1,21 @@
 from neo4j import GraphDatabase, RoutingControl
+import pandas as pd
 
 
 URI = "neo4j://localhost:7687"
-AUTH = ("neo4j", "password")
+AUTH = ("neo4j", "Password1984")
 
-def print_friends(driver, name):
+query = ''' MATCH (c:Character)-[:IN_SCENE]->(s:Scene)-[:PART_OF_EPISODE]->(e:Episode)
+RETURN e.title AS Episode, c AS CharacterCount
+ORDER BY CharacterCount DESC;'''
+
+def get_data(driver):
     records, _, _ = driver.execute_query(
-        "MATCH p=()-[r:PART_OF_EPISODE]->() RETURN p LIMIT 25",
-        name=name, database_="neo4j", routing_=RoutingControl.READ,
+        query, database_="neo4j", routing_=RoutingControl.READ,
     )
     for record in records:
         print(record)
 
-
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    print(driver)
+if __name__ == '__main__':
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        get_data(driver)
